a9266438eaef1b224a9ea007a4e312a20a9b0a6e
Patrick Bowen <phunanon@users.noreply.github.com>
2025-06-24 02:13:13 +0100

    Initial version

---
+++ main.cpp
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <ranges>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <functional>
+#include <ranges>
+using namespace std;
+
+struct Procedure
+{
+    int line;
+    string name;
+    vector<string> ins;
+    vector<string> outs;
+
+    Procedure(int line, string name, vector<string> ins, vector<string> outs)
+        : line(line), name(name), ins(ins), outs(outs) {}
+};
+
+#define THROW_SIGNAL_NOT_FOUND(signal)                           \
+    {                                                            \
+        cout << "Line " << p.line + 1 << " available signals: "; \
+        for (const auto &pair : signals)                         \
+            cout << pair.first << " ";                           \
+        throw runtime_error("Input " + signal + " not found");   \
+    }
+void DebugSignals(const map<std::string, bool> signals)
+{
+    cout << "Signals: " << endl;
+    for (auto &s : signals)
+    {
+        cout << " " << s.second << " " << s.first << endl;
+    }
+}
+
+struct Chip
+{
+    vector<string> ins;
+    vector<string> outs;
+    vector<Procedure> procedures;
+
+    vector<bool> execute(const map<string, Chip> &chips, const vector<bool> &inputs) const
+    {
+        map<string, bool> signals;
+        for (const auto &out : outs)
+            signals[out] = false;
+        for (size_t i = 0; i < ins.size(); ++i)
+            signals[ins[i]] = inputs[i];
+        for (const auto &p : procedures)
+        {
+            if (p.name == "->")
+            {
+                if (p.ins.size() != p.outs.size())
+                    throw runtime_error("Line " + to_string(p.line + 1) + ": imbalanced");
+                for (size_t j = 0; j < p.ins.size(); ++j)
+                {
+                    auto it = signals.find(p.ins[j]);
+                    if (it == signals.end())
+                        THROW_SIGNAL_NOT_FOUND(p.ins[j])
+                    signals[p.outs[j]] = it->second;
+                }
+                continue;
+            }
+            else if (p.name == "NAND")
+            {
+                if (p.ins.size() != 2 || p.outs.size() != 1)
+                    throw runtime_error("Line " + to_string(p.line + 1) + ": invalid NAND");
+                // cout << "Executing chip NAND with inputs: " << signals[p.ins[0]] << " " << signals[p.ins[1]] << endl;
+                signals[p.outs[0]] = !(signals[p.ins[0]] && signals[p.ins[1]]);
+                // cout << " NAND outputs: " << signals[p.outs[0]] << endl;
+                continue;
+            }
+            auto chip = chips.find(p.name);
+            if (chip == chips.end())
+                throw runtime_error("Line " + to_string(p.line + 1) + ": chip " + p.name + " not found");
+            vector<bool> chipInputs;
+            for (const auto &in : p.ins)
+            {
+                if (in == "0" || in == "1")
+                {
+                    chipInputs.push_back(in == "1");
+                    continue;
+                }
+                auto it = signals.find(in);
+                if (it == signals.end())
+                    THROW_SIGNAL_NOT_FOUND(in)
+                chipInputs.push_back(it->second);
+            }
+            // cout << "Executing chip " << p.name << " with inputs: ";
+            // for (bool input : chipInputs)
+            //     cout << (input ? 1 : 0) << " ";
+            // cout << endl;
+            vector<bool> chipOutputs = chip->second.execute(chips, chipInputs);
+            // cout << " " << p.name << " outputs: ";
+            // for (bool output : chipOutputs)
+            //     cout << (output ? 1 : 0) << " ";
+            // cout << endl;
+            for (size_t j = 0; j < p.outs.size(); ++j)
+                signals[p.outs[j]] = chipOutputs[j];
+        }
+        vector<bool> outputs(outs.size());
+        for (size_t i = 0; i < outs.size(); ++i)
+            if (signals.find(outs[i]) != signals.end())
+                outputs[i] = signals[outs[i]];
+        // DebugSignals(signals);
+        return outputs;
+    }
+};
+
+void EchoFile(const map<string, Chip> &chips);
+
+vector<string> readFileToVector(const string &filename)
+{
+    ifstream source;
+    source.open(filename);
+    vector<string> lines;
+    string line;
+    while (getline(source, line))
+    {
+        lines.push_back(line);
+    }
+    source.close();
+    return lines;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        throw runtime_error("Usage: " + string(argv[0]) + " <filename> [--echo-file]");
+    }
+    string filename(argv[1]);
+    vector<string> lines = readFileToVector(filename);
+    map<string, Chip> chips;
+
+    bool newChip = true;
+    bool readIns = false;
+    bool readProc = false;
+    bool readOuts = false;
+    string chipName, procName;
+    for (int l = 0; l < lines.size(); ++l)
+    {
+        string line = lines[l];
+        string nextLine = (l + 1 < lines.size()) ? lines[l + 1] : "";
+        if (line == "")
+        {
+            newChip = true;
+        }
+        else if (newChip)
+        {
+            chipName = line;
+            chips[chipName] = Chip();
+            newChip = false;
+            readIns = true;
+        }
+        else if (readIns || readOuts)
+        {
+            stringstream ss(line);
+            string chunk;
+            while (getline(ss, chunk, ' '))
+                if (chunk != "")
+                    if (readIns)
+                        chips[chipName].ins.push_back(chunk);
+                    else if (readOuts)
+                        chips[chipName].outs.push_back(chunk);
+            if (nextLine[0] == ' ' || nextLine[0] == '\0')
+            {
+                readProc = readIns;
+                readIns = false;
+                readOuts = false;
+            }
+        }
+        else if (readProc)
+        {
+            Procedure proc(l, "", vector<string>(), vector<string>());
+            bool readProcName = true;
+            bool readProcIns = false;
+            bool readProcOuts = false;
+            stringstream ss(line);
+            string chunk;
+            while (getline(ss, chunk, ' '))
+            {
+                if (chunk == "")
+                {
+                    if (proc.ins.size() && readProcIns)
+                    {
+                        readProcIns = false;
+                        readProcOuts = true;
+                    }
+                }
+                else if (readProcName)
+                {
+                    if (chunk != "\"")
+                        procName = chunk;
+                    proc.name = procName;
+                    readProcName = false;
+                    readProcIns = true;
+                }
+                else if (readProcIns)
+                {
+                    proc.ins.push_back(chunk);
+                }
+                else if (readProcOuts)
+                {
+                    proc.outs.push_back(chunk);
+                }
+            }
+            chips[chipName].procedures.push_back(proc);
+            if (nextLine[0] != ' ')
+            {
+                readProc = false;
+                readOuts = true;
+            }
+        }
+    }
+
+    if (argc == 3 && string(argv[2]) == "--echo-file")
+        EchoFile(chips);
+
+    vector<bool> outs = chips["Main"].execute(chips, {false});
+    for (bool out : outs)
+        cout << (out ? 1 : 0);
+    cout << endl;
+
+    return 0;
+}
+
+void EchoFile(const map<string, Chip> &chips)
+{
+    for (const auto &pair : chips)
+    {
+        cout << pair.first << '(' << pair.second.ins.size() << ')' << endl;
+        for (const auto &in : pair.second.ins)
+            cout << in << " ";
+        cout << endl;
+        for (const auto &proc : pair.second.procedures)
+        {
+            cout << "    " << proc.name << "  ";
+            for (const auto &in : proc.ins)
+                cout << in << " ";
+            cout << "  ";
+            for (const auto &out : proc.outs)
+                cout << out << " ";
+            cout << endl;
+        }
+        for (const auto &out : pair.second.outs)
+            cout << out << " ";
+        cout << endl
+             << endl;
+    }
+}
